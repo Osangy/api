6d408480d1d13cc41d7fd345e69a7fd31908f3a0
Adrien Dulong <adrien.dulong@gmail.com>
2016-12-14 17:58:25 +0100

    Fix

--- schema.js
+++ schema.js
@@ -37,7 +37,8 @@ const rootResolvers = {
     message(root, {limit, conversationId}, context) {
       console.log("Facebook page id requesting : ", context.pageId);
       const limitValidator = (limit > 30) ? 30 : limit;
-      return Message.find({conversation: conversationId}).sort({seq : -1}).limit(limit).reverse();
+      let messages = Message.find({conversation: conversationId}).sort({seq : -1}).limit(limit)
+      return messages.reverse();
     },
     conversation(root, { limit }, context){
       const limitValidator = (limit > 20) ? 10 : limit;
